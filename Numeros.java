cc767a6b40f60a9dcfe708aea8f65d9357ccbded
Sanderson Rhawan da Silva Torres <106162224+Darknios@users.noreply.github.com>
2023-07-20 16:27:21 -0300

    Add files via upload

---
+++ Numeros.java
@@ -0,0 +1,99 @@
+package encapsulamento;
+
+public class Numeros {
+ private int numero1;
+ private int numero2;
+ private int numero3;
+ private int numero4;
+ private int numero5;
+ private int numero6;
+ private int numero7;
+ private int numero8;
+ private int numero9;
+ private int numero10;
+ public Numeros () {
+	 
+ }
+ public int getnumero1() {
+	 return numero1;
+ }
+
+ public void setnumero1(int numero1) {
+	this.numero1 = numero1;
+}
+ 
+public int getnumero2() {
+	 return numero2;
+}
+
+public void setnumero2(int numero2) {
+	this.numero2 = numero2;
+}
+public int getnumero3() {
+	 return numero3;
+}
+
+public void setnumero3(int numero3) {
+	this.numero3 = numero3;
+}
+
+public int getnumero4() {
+	 return numero4;
+}
+
+public void setnumero4(int numero4) {
+	this.numero10 = numero4;
+}
+
+public int getnumero5() {
+	 return numero5;
+}
+
+public void setnumero5(int numero5) {
+	this.numero5 = numero5;
+}
+
+ 
+public int getnumero6() {
+	 return numero6;
+}
+
+public void setnumero6(int numero6) {
+	this.numero6 = numero6;
+}
+
+public int getnumero7() {
+	 return numero7;
+}
+
+public void setnumero7(int numero7) {
+	this.numero7 = numero7;
+}
+
+public int getnumero8() {
+	 return numero8;
+}
+
+public void setnumero8(int numero8) {
+	this.numero8 = numero8;
+}
+
+public int getnumero9() {
+	 return numero9;
+}
+
+public void setnumero9(int numero9) {
+	this.numero9 = numero9;
+}
+
+public int getnumero10() {
+	 return numero10;
+}
+
+public void setnumero10(int numero10) {
+	this.numero5 = numero10;
+}
+}
+ 
+ 
+ 
---
+++ usandoatributos.java
@@ -0,0 +1,37 @@
+package encapsulamento;
+
+public class usandoatributos{
+ public static void main (String [] args){
+	Numeros n1 = new Numeros();
+	Numeros n2 = new Numeros();
+	Numeros n3 = new Numeros();
+	Numeros n4 = new Numeros();
+	Numeros n5 = new Numeros();
+	Numeros n6 = new Numeros();
+	Numeros n7 = new Numeros();
+	Numeros n8 = new Numeros();
+	Numeros n9 = new Numeros();
+	Numeros n10 = new Numeros();
+	
+	n1.setnumero1(10);
+	System.out.println(n1.getnumero1());
+	n2.setnumero2(20);
+	System.out.println(n2.getnumero2());
+	n3.setnumero3(30);
+	System.out.println(n3.getnumero3());
+	n4.setnumero4(40);
+	System.out.println(n4.getnumero4());
+	n5.setnumero5(50);
+	System.out.println(n5.getnumero5());
+	n6.setnumero6(60);
+	System.out.println(n6.getnumero6());
+	n7.setnumero7(70);
+	System.out.println(n7.getnumero7());
+	n8.setnumero8(80);
+	System.out.println(n8.getnumero8());
+	n9.setnumero9(90);
+	System.out.println(n9.getnumero9());
+	n10.setnumero10(100);
+	System.out.println(n10.getnumero10());
+ }
+}
